d0e2d091e7d38a5bce6faeba5489e25f478d4cc7
cottonatocd <cotton@ocdagency.com>
2019-08-04 18:04:54 -0400

    press page carousel

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@ let number = 0;
 let target = 4.1;
 let increaseNumFired = false;
 
+function normalizeSlideHeights() {
+    $('.carousel').each(function(){
+      var items = $('.carousel-item', this);
+      // reset the height
+      items.css('min-height', 0);
+      // set the height
+      var maxHeight = Math.max.apply(null, 
+          items.map(function(){
+              return $(this).outerHeight()}).get() );
+      items.css('min-height', maxHeight + 'px');
+    })
+}
+
+$(window).on(
+    'load resize orientationchange', 
+    normalizeSlideHeights);
+
 
 $('#increasingNumber span').text('0');
 // var interval = setInterval(function() {
